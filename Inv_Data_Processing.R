7f6b0abaeb5783764ff621b1d8c8f0a7a965469e
EldonRose <rose@eldonfarms.com>
2026-06-25 15:13:39 -0400

    Data processing and map test

--- Inv_Data_Processing.R
+++ Inv_Data_Processing.R
@@ -2,17 +2,43 @@
 setwd("C:/Users/rwetz/Documents/GitHub/Forest_Inventory")
 
 # Packages
-install.packages("pdftools")
-library(pdftools)
+library(dplyr)
+library(data.table)
 
 # Load and clean up data
 tree <- read.csv("C:/Users/rwetz/Documents/GitHub/Forest_Inventory/TreePlot_All.csv")
-os <- subset(tree, tree$Strata == "O")
+sa <- read.csv("C:/Users/rwetz/Documents/GitHub/Forest_Inventory/Saplings_All.csv")
+sh <- read.csv("C:/Users/rwetz/Documents/GitHub/Forest_Inventory/Shrubs_All.csv")
+
+# Standardize with sp focus
+  # Create relative frequency table
+    # Trees
+treefreak <- tree %>%
+  group_by (Plot.ID, Species) %>%
+  summarise (n=n()) %>%
+  mutate(rel.freq = paste0(round(100 * n/sum(n), 0), "%"))
+treefreak$Class <- "Canopy"
+write.csv(treefreak, "C:/Users/rwetz/Documents/GitHub/Forest_Inventory/TreeFreq.csv")
+
+  # Saplings
+sa$Sp.total <- rowSums(cbind(sa$X0.1.3..DBH,sa$X3.4.9..DBH), na.rm=TRUE) 
+sa$Class <- "Understory"
+
+safr <- sa %>%
+  group_by (Plot.ID, Species) %>%
+  summarise (n=n()) %>%
+  mutate(rel.freq = paste0(round(100 * n/sum(n), 0), "%"))
+
+# Shrubs
+sh$Class <- "Shrub"
+colnames(sh)[colnames(sh) == 'X..Cover'] <- 'rel.freq'
+
 locs <- read.csv("C:/Users/rwetz/Documents/GitHub/Forest_Inventory/PlotLocs.csv")
 loc.cl <- locs[c("OBJECTID", "Name", "X", "Y")]
 
 # Retrieve plot names
 plots <- loc.cl[["Name"]]
 
-list_df <- split(tree, tree$Plot.ID)
-list2env(list_df, envir = .GlobalEnv)
+#os <- subset(tree, tree$Strata == "O")
+# list_df <- split(tree, tree$Plot.ID)
+# list2env(list_df, envir = .GlobalEnv)
