204ca7fd980ab8d44da2f75d33e648f76ed20c32
EldonRose <rose@eldonfarms.com>
2026-07-15 16:03:33 -0400

    Data formatting
    Figured out transposition code and started manually adding missing dates

--- Inv_Data_Processing.R
+++ Inv_Data_Processing.R
@@ -2,8 +2,11 @@
 setwd("C:/Users/rwetz/Documents/GitHub/Forest_Inventory")
 
 # Packages
+install.packages("tidyverse")
+
 library(dplyr)
 library(data.table)
+library(tidyverse)
 
 # Load and clean up data
 tree <- read.csv("C:/Users/rwetz/Documents/GitHub/Forest_Inventory/TreePlot_All.csv")
@@ -19,24 +22,28 @@ sh <- read.csv("C:/Users/rwetz/Documents/GitHub/Forest_Inventory/Shrubs_All.csv"
 #   mutate(rel.freq = paste0(round(100 * n/sum(n), 0), "%"))
 #treefreak$Class <- "Canopy"
 
-  # Decimals
-treefreak <- tree %>%
-  group_by (Plot.ID, Species) %>%
-  summarise (n=n()) %>%
-  mutate(rel.freq = paste0((n/sum(n))))
-treefreak$Class <- "Canopy"
-  # Remove NAs
-treefreak <- na.omit(treefreak)
-write.csv(treefreak, "C:/Users/rwetz/Documents/GitHub/Forest_Inventory/TreeRF_Dec.csv")
+  # Convert Percents to Decimals
+# treefreak <- tree %>%
+#   group_by (Plot.ID, Species) %>%
+#   summarise (n=n()) %>%
+#   mutate(rel.freq = paste0((n/sum(n))))
+# treefreak$Class <- "Canopy"
+#   # Remove NAs
+# treefreak <- na.omit(treefreak)
+# write.csv(treefreak, "C:/Users/rwetz/Documents/GitHub/Forest_Inventory/TreeRF_Dec.csv")
 
-list_df <- split(treefreak, treefreak$Plot.ID)
-list2env(list_df, envir = .GlobalEnv)
-list2DF(list_df)
+treefreak <- read.csv("C:/Users/rwetz/Documents/GitHub/Forest_Inventory/TreeRF_Dec.csv")
 
-for (i in list_df) {
-  i <- t(i)
-  }
+# Turn data into a list by plot
+  #list_df <- split(treefreak, treefreak$Plot.ID)
+  #list2env(list_df, envir = .GlobalEnv)
+  #list2DF(list_df)
 
+# Transpose each df in list and save 
+  # list_df <- lapply(list_df, function(x) {t(x)})
+  # list_df <- lapply(list_df, function(x) {as.data.frame(x)})
+  # trees <- as.data.frame(list_df)
+  # write_csv(trees, "C:/Users/rwetz/Documents/GitHub/Forest_Inventory/TreeRF_Cols.csv")
 
   # Saplings
 sa$Sp.total <- rowSums(cbind(sa$X0.1.3..DBH,sa$X3.4.9..DBH), na.rm=TRUE) 
